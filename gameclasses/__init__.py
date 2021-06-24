c47d78dc0a4ce63760bba6e9ea35a714a2cc140d
Manuel Staufer <tuxlergamer@gmail.com>
2021-06-24 12:28:37 +0200

    Delete gameclasses directory

--- gameclasses/__init__.py
+++
@@ -1,4 +0,0 @@
-from gameclasses.player import Player2D, Player3D
-from gameclasses.inventory import Inventory
-from gameclasses.location import Location2D, Location3D
-from gameclasses.entity import Entity2D, Entity3D
